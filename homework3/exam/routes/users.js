4e9931c88adf4f357f994e558e69a203209bb58a
HyeonJooo <parkhj2416@gmail.com>
2020-05-23 01:19:55 +0900

    hw3 update

--- homework3/exam/routes/users.js
+++ homework3/exam/routes/users.js
@@ -111,4 +111,24 @@ router.post('/signin', async (req, res) => {
 
 
 // });
+
+router.get('/profile/:id', async (req, res) => {
+  // request params 에서 데이터 가져오기
+  const id = req.params.id;
+  const user = User.filter(user => user.id == id)[0];
+  // 존재하는 아이디인지 확인 - 없다면 No user 반환
+  if (user === undefined) {
+      res.status(statusCode.BAD_REQUEST)
+          .send(util.fail(statusCode.BAD_REQUEST, resMessage.NO_USER));
+      return;
+  }
+  const dto = {
+      id: user.id,
+      name: user.name,
+      email: user.email
+  }
+  // 성공 - login success와 함께 user Id 반환
+  res.status(statusCode.OK)
+      .send(util.success(statusCode.OK, resMessage.READ_PROFILE_SUCCESS, dto));
+});
 module.exports = router;
