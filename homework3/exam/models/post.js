821483329fba6cd3e3bf1f60059fe551bfe720be
HyeonJooo <parkhj2416@gmail.com>
2020-06-02 11:35:55 +0900

    HW3 level3

---
+++ homework3/exam/models/post.js
@@ -0,0 +1,18 @@
+var post = [
+    {
+        postIdx: '0',
+        author: 'hyeonju',
+        title: 'homework3',
+        content: '레벨3',
+        date : '2020년 6월 2일'
+    },
+    {
+        postIdx: '1',
+        author: 'park',
+        title: 'sopt',
+        content: 'our sopt 서버파트',
+        date: '2020년 06월 3일'
+    }
+];
+
+module.exports = post;
--- homework3/exam/routes/index.js
+++ homework3/exam/routes/index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/user', require('./user'));
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.use('/user',require('./user'));
+router.use('/post',require('./post'));
+
 module.exports = router;
