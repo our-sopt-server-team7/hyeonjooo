7ef3e6822233ced1819b845e294932979041433e
HyeonJooo <parkhj2416@gmail.com>
2020-06-16 17:41:04 +0900

    hw5 level2 img

--- homework5/mission/modules/jwt.js
+++ homework5/mission/modules/jwt.js
@@ -1,7 +1,7 @@
 const randToken = require('rand-token');
 const jwt = require('jsonwebtoken');
-const secretKey = require('../config/secretKey').secretKey;
-const options = require('../config/secretKey').options;
+const secretKey = require('../config/secretkey').secretKey;
+const options = require('../config/secretkey').options;
 const TOKEN_EXPIRED = -3;
 const TOKEN_INVALID = -2;
 
