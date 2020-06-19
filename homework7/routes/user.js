82351708c56c2384d8e05af7cb8aee9987f6b9bc
HyeonJooo <parkhj2416@gmail.com>
2020-06-19 17:07:51 +0900

    hw7 level1

--- homework7/routes/user.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const userController = require('../controller/userController');
-
-router.post('/signup', userController.signup);
-router.post('/signin', userController.signin);
-router.post('/getUserById', userController.getUserById);
-
-module.exports = router;
