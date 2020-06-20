66f638fe3166c3e0bf5a39769c2099a8db9ba0d0
HyeonJooo <parkhj2416@gmail.com>
2020-06-20 13:18:03 +0900

    hw7 level3

--- homework7/practice/practice/routes/user.js
+++ homework7/practice/practice/routes/user.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const UserController = require('../controllers/user');
 const AuthMiddleware = require('../middlewares/auth');
 
-// const upload = require('../modules/multer');//이건 s3연결할 때
+const upload = require('../modules/multer');//이건 s3연결할 때
 
-const multer = require('multer');
-const upload = multer({
-    dest: 'upload/'
-});//s3연결할 때는 주석처리
+// const multer = require('multer');
+// const upload = multer({
+//     dest: 'upload/'
+// });//s3연결할 때는 주석처리
 
 router.post('/signup', UserController.signup);
 router.post('/signin', UserController.signin);
@@ -22,5 +22,5 @@ router.post('/signin', UserController.signin);
     RESPONSE DATA : user profile
 */
 router.post('/profile', AuthMiddleware.checkToken, upload.single('profile'), UserController.updateProfile);
-
+router.post('/selfies', AuthMiddleware.checkToken, upload.array('images', 3), UserController.array);
 module.exports = router;
